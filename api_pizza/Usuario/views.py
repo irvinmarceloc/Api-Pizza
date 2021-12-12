2abd152ba0650e79b219e0032ee5a5b3ee08204d
irvinmarceloc <irvinmarceloc@gmail.com>
2021-12-12 17:39:51 -0300

    Vista de usuarios

--- api_pizza/Usuario/views.py
+++ api_pizza/Usuario/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, serializers
 from .models import Usuario
 from .serializers import UsuarioSerializer
 
 class UsuarioList(generics.ListCreateAPIView):
-    
+    queryset = Usuario.objects.all() 
+    serializers_class = UsuarioSerializer
 
