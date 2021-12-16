1bf33de969f6f1008a0c3b0abadcbf53c60d164b
irvinmarceloc <irvinmarceloc@gmail.com>
2021-12-16 03:57:16 -0300

    Administracion de la aplicacon

--- api_pizza/ingrediente/admin.py
+++ api_pizza/ingrediente/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from ingrediente.models import *
 
-# Register your models here.
+##Para que el usuario pueda administrar las tablas desde el panel
+
+class IngredienteAdmin(admin.ModelAdmin):
+    list_display=("nombre", "categoria") #Para ver con formatos
+    nombre=models.CharField(max_length=30)
+    categoria=models.BooleanField()
+
+
+admin.site.register(Ingrediente, IngredienteAdmin)
