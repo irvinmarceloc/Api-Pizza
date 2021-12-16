43647a92c7cbbe6ec690aa0ef529cfeceb721482
irvinmarceloc <irvinmarceloc@gmail.com>
2021-12-16 03:45:21 -0300

    Modelo Pizaa creado

--- api_pizza/api_pizza/settings.py
+++ api_pizza/api_pizza/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'Usuario',
     'rest_framework.authtoken',
+    'pizza',
 ]
 
 
--- api_pizza/pizza/models.py
+++ api_pizza/pizza/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+#Modelo de piza
+class Pizza(models.Model):
+    nombre=models.CharField(max_length=30)
+    precio=models.IntegerField()
+    activo=models.BooleanField()
+
+    def __str__(self):
+        return self.nombre
+        
