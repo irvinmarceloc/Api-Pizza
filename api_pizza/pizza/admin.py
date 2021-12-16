005178f09978b41dabdbfad29a01e9d477161ab2
irvinmarceloc <irvinmarceloc@gmail.com>
2021-12-16 03:49:41 -0300

    para administracion de modelos

--- api_pizza/pizza/admin.py
+++ api_pizza/pizza/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from pizza.models import *
 
-# Register your models here.
+##Para que el usuario pueda administrar las tablas desde el panel
+
+#Administrar Pizzas
+class PizzaAdmin(admin.ModelAdmin):
+    list_display=("nombre", "precio", "activo") #Para ver con formatos
+    search_fields=("nombre","activo") #Para filtrar por buscador
+
+admin.site.register(Pizza, PizzaAdmin)
