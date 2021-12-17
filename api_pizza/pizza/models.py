fc7e7b35c9dec180cae1f4808b73c74d58cf609d
irvinmarceloc <irvinmarceloc@gmail.com>
2021-12-16 23:46:28 -0300

    Relacion mucho a mucho Pizza-Ingrediente

--- api_pizza/pizza/models.py
+++ api_pizza/pizza/models.py
@@ -1,11 +1,14 @@
 from django.db import models
+from ingrediente.models import Ingrediente
 
 #Modelo de piza
 class Pizza(models.Model):
     nombre=models.CharField(max_length=30)
+    ingredientes= models.ManyToManyField(Ingrediente)
     precio=models.IntegerField()
     activo=models.BooleanField()
 
     def __str__(self):
         return self.nombre
-        
+
+##Relacionamos Pizza e Ingrediente mucho a mucho 
