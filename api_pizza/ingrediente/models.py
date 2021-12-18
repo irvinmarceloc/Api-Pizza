5913e7b7188efa282e0f14fcc946b19c5a04c472
irvinmarceloc <irvinmarceloc@gmail.com>
2021-12-17 22:24:02 -0300

    modificar ingredientes BD y modelos

--- api_pizza/ingrediente/models.py
+++ api_pizza/ingrediente/models.py
@@ -2,5 +2,6 @@ from django.db import models
 
 #Modelo Ingrediente
 class Ingrediente(models.Model):
+    id = models.IntegerField(primary_key=True)
     nombre=models.CharField(max_length=30)
-    categoria=models.BooleanField()
+    
