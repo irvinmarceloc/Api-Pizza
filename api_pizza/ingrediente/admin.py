f92e0319a43f0d01386ddb363796f941727c744f
irvinmarceloc <irvinmarceloc@gmail.com>
2021-12-17 22:25:06 -0300

    vista cambiada

--- api_pizza/ingrediente/admin.py
+++ api_pizza/ingrediente/admin.py
@@ -4,9 +4,8 @@ from ingrediente.models import *
 ##Para que el usuario pueda administrar las tablas desde el panel
 
 class IngredienteAdmin(admin.ModelAdmin):
-    list_display=("nombre", "categoria") #Para ver con formatos
+    list_display=("nombre", "id") #Para ver con formatos
     nombre=models.CharField(max_length=30)
-    categoria=models.BooleanField()
-
+    id =models.BooleanField()
 
 admin.site.register(Ingrediente, IngredienteAdmin)
