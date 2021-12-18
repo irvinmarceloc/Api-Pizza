8fd8acd47caa48bdfcbb111f4d60c64d717c5840
irvinmarceloc <irvinmarceloc@gmail.com>
2021-12-17 22:16:24 -0300

    configuraciones de autenticacion y nuevo superuser

--- api_pizza/api_pizza/settings.py
+++ api_pizza/api_pizza/settings.py
@@ -53,6 +53,12 @@ REST_FRAMEWORK = {
     ##Verifica que la sesión este activa
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [ 
+        'rest_framework.authentication.TokenAuthentication',
     ]
 }
 
